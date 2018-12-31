2f24064399faf08531184c7719c05022bae5495b
Hajime Murao <2620990+hjmr@users.noreply.github.com>
2018-12-31 11:07:39 +0900

    network structure modified.

--- MyNet.py
+++ MyNet.py
@@ -4,21 +4,25 @@ import chainer.functions as F
 from chainer.backends import cuda
 
 
-CONV_FILTER_SIZE = 4
-MAX_POOLING_SIZE = 3
-NUM_HIDDEN_NEURONS = 2048
+CONV_FILTER_SIZE = 3
+MAX_POOLING_SIZE = 2
+
+NUM_HIDDEN_NEURONS1 = 4096
+NUM_HIDDEN_NEURONS2 = 1024
 
 
 class MySubNet(chainer.Chain):
     def __init__(self):
         super(MySubNet, self).__init__()
         with self.init_scope():
-            self.conv11 = L.Convolution2D(24, CONV_FILTER_SIZE)
-            self.conv12 = L.Convolution2D(24, CONV_FILTER_SIZE)
+            self.conv11 = L.Convolution2D(32, CONV_FILTER_SIZE)
+            self.conv12 = L.Convolution2D(32, CONV_FILTER_SIZE)
             self.conv21 = L.Convolution2D(48, CONV_FILTER_SIZE)
             self.conv22 = L.Convolution2D(48, CONV_FILTER_SIZE)
-            self.conv31 = L.Convolution2D(96, CONV_FILTER_SIZE)
-            self.conv32 = L.Convolution2D(96, CONV_FILTER_SIZE)
+            self.conv31 = L.Convolution2D(64, CONV_FILTER_SIZE)
+            self.conv32 = L.Convolution2D(64, CONV_FILTER_SIZE)
+            self.conv41 = L.Convolution2D(96, CONV_FILTER_SIZE)
+            self.conv42 = L.Convolution2D(96, CONV_FILTER_SIZE)
 
     def forward(self, x):
         h = F.relu(self.conv11(x))
@@ -30,6 +34,9 @@ class MySubNet(chainer.Chain):
         h = F.relu(self.conv31(h))
         h = F.relu(self.conv32(h))
         h = F.max_pooling_2d(h, ksize=MAX_POOLING_SIZE)
+        h = F.relu(self.conv41(h))
+        h = F.relu(self.conv42(h))
+        h = F.max_pooling_2d(h, ksize=MAX_POOLING_SIZE)
         return h
 
 
@@ -40,8 +47,9 @@ class MyNet(chainer.Chain):
             self.convNets = []
             for i in range(image_num):
                 self.convNets.append(MySubNet())
-            self.fullLayer1 = L.Linear(in_size=None, out_size=NUM_HIDDEN_NEURONS)
-            self.fullLayer2 = L.Linear(in_size=None, out_size=out_size)
+            self.fullLayer1 = L.Linear(in_size=None, out_size=NUM_HIDDEN_NEURONS1)
+            self.fullLayer2 = L.Linear(in_size=None, out_size=NUM_HIDDEN_NEURONS2)
+            self.fullLayer3 = L.Linear(in_size=None, out_size=out_size)
         self.gpuid = gpuid
         self.image_num = image_num
 
@@ -57,9 +65,17 @@ class MyNet(chainer.Chain):
         for net in self.convNets:
             net.to_intel64()
 
-    def forward(self, x):
+    def calc_cnn_layer(self, x):
         x_inp = F.split_axis(x, self.image_num, axis=2)
         h = [self.convNets[i].forward(x_inp[i]) for i in range(self.image_num)]
+        return h
+
+    def calc_ffnn_layer(self, x):
+        h = F.dropout(F.relu(self.fullLayer1(x)))
+        h = F.dropout(F.relu(self.fullLayer2(h)))
+        return self.fullLayer3(h)
+
+    def forward(self, x):
+        h = self.calc_cnn_layer(x)
         h = F.concat(h, axis=2)
-        h = F.dropout(F.relu(self.fullLayer1(h)))
-        return self.fullLayer2(h)
+        return self.calc_ffnn_layer(h)
