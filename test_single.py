0d51c832a9f40e5e5f6faa14f05ce78e2e7ebb84
Hajime Murao <2620990+hjmr@users.noreply.github.com>
2019-01-23 20:24:37 +0900

    Make test_single.py working.

--- test_single.py
+++ test_single.py
@@ -1,9 +1,11 @@
 import sys
 import argparse
 
-import numpy as np
+import numpy
 import chainer
 from chainer import serializers
+import chainer.functions as F
+from chainer.backends import cuda
 
 from MyNet import MyNet
 from read_data import read_one_image
@@ -17,6 +19,8 @@ def parse_arg():
                         help='the number of images as a set of input.')
     parser.add_argument('-m', '--model_file', type=str, nargs=1,
                         help='load specified model file.')
+    parser.add_argument('-g', '--gpuid', type=int, default=-1,
+                        help='GPU ID for calculation.')
     parser.add_argument('images', type=str, nargs='+',
                         help='images for testing')
     return parser.parse_args()
@@ -26,6 +30,12 @@ def main():
     args = parse_arg()
 
     model = MyNet(args.out_class_num, args.image_num)
+    xp = numpy
+    if 0 <= args.gpuid:
+        cuda.get_device_from_id(args.gpuid).use()
+        model.to_gpu()
+        xp = cuda.cupy
+
     serializers.load_npz(args.model_file[0], model)
 
     test_images = []
@@ -34,13 +44,14 @@ def main():
 
     test_data = []
     for i in range(len(test_images) - (args.image_num - 1)):
-        test_data.append(np.concatenate(test_images[i:i + args.image_num]))
+        test_data.append(numpy.concatenate(test_images[i:i + args.image_num]))
 
     with chainer.configuration.using_config('train', False):
         with chainer.using_config('enable_backprop', False):
-            y = model.forward(test_data)
-            pred = np.argmax(y)
-            print(pred)
+            for i in range(len(test_data)):
+                y = model.forward(xp.array([test_data[i]]))
+                pred = F.argmax(y)
+                print(pred.data)
 
 
 if __name__ == '__main__':
